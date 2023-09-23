c02c85bc3807192d263ed9d1663120383cf0c1b8
Taejun Han <124488773+han-tomas@users.noreply.github.com>
2023-09-23 18:48:10 +0900

    [Bronze III] Title: 오븐 시계, Time: 232 ms, Memory: 18396 KB -BaekjoonHub

---
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int A = scan.nextInt();
+        int B = scan.nextInt();
+        int C = scan.nextInt();
+        if(B+C>=60)
+        {
+            if((A+((B+C)/60))>=24)
+               System.out.println(((A+((B+C)/60))-24)+" "+((B+C)%60));
+            else   
+                System.out.println((A+((B+C)/60))+" "+((B+C)%60));
+        }    
+        else
+        {
+            System.out.println(A+" "+(B+C));
+        }
+        
+    }
+}
