6e4f4b3dd49ee6fc9ce8e5a56d18d981e3abef5c
Taejun Han <124488773+han-tomas@users.noreply.github.com>
2023-10-05 16:34:58 +0900

    [Bronze II] Title: 벌집, Time: 128 ms, Memory: 14340 KB -BaekjoonHub

---
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -0,0 +1,20 @@
+import java.io.*;
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int count = 1;
+        int range = 2; // 2부터 시작
+        if (N == 1) {
+			System.out.print(1);
+		}
+ 
+		else {
+			while (range <= N) {	// 범위가 N보다 커지기 직전까지 반복 
+				range = range + (6 * count);	// 다음 범위의 최솟값으로 초기화 
+				count++;	// count 1 증가 
+			}
+			System.out.print(count);
+		}
+    }
+}
